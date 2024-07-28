44531575742c68be3b634f461c140f9f0c973f87
baby <gkje123@naver.com>
2024-07-28 21:43:03 +0900

    initial commit

---
+++ src/main/java/com/example/qnacomunity/QnaComunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.qnacomunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QnaComunityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QnaComunityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/qnacomunity/QnaComunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qnacomunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QnaComunityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
