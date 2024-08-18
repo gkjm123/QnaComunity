4682079d7a3418e39bcf4e3e0cb5964a6560ea57
baby <gkje123@naver.com>
2024-08-19 00:42:20 +0900

    feat: 단어/키워드 검색시 sort 기능 pageable 이용하도록 변경

--- src/main/java/com/example/qnacomunity/controller/SearchController.java
+++ src/main/java/com/example/qnacomunity/controller/SearchController.java
@@ -1,13 +1,13 @@
 package com.example.qnacomunity.controller;
 
 import com.example.qnacomunity.service.ElasticSearchService;
-import com.example.qnacomunity.type.SearchOrder;
 import com.example.qnacomunity.type.SearchRange;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,24 +23,36 @@ public class SearchController {
 
   private final ElasticSearchService elasticSearchService;
 
-  @Operation(summary = "질문 검색(제목/내용)")
-  @GetMapping("/word")
-  public ResponseEntity<?> searchWord(
+  @Operation(summary = "질문 검색(제목/내용, 정확도순)")
+  @GetMapping("/related-word")
+  public ResponseEntity<?> searchWordByRelated(
       @PageableDefault Pageable pageable,
       @NotBlank(message = "검색어를 입력해주세요.") @RequestParam String word,
-      @NotNull(message = "정렬 순서 미지정") @RequestParam SearchOrder searchOrder,
       @NotNull(message = "검색 범위 미지정") @RequestParam SearchRange searchRange
   ) {
 
     return ResponseEntity.ok(
-        elasticSearchService.searchWord(pageable, word, searchOrder, searchRange)
+        elasticSearchService.searchWord(pageable, word, searchRange)
+    );
+  }
+
+  @Operation(summary = "질문 검색(제목/내용, 최신순)")
+  @GetMapping("/latest-word")
+  public ResponseEntity<?> searchWordByLatest(
+      @PageableDefault(sort = "created", direction = Direction.DESC) Pageable pageable,
+      @NotBlank(message = "검색어를 입력해주세요.") @RequestParam String word,
+      @NotNull(message = "검색 범위 미지정") @RequestParam SearchRange searchRange
+  ) {
+
+    return ResponseEntity.ok(
+        elasticSearchService.searchWord(pageable, word, searchRange)
     );
   }
 
   @Operation(summary = "질문 검색(키워드)")
   @GetMapping("/keyword")
   public ResponseEntity<?> searchKeyword(
-      @PageableDefault Pageable pageable,
+      @PageableDefault(sort = "created", direction = Direction.DESC) Pageable pageable,
       @NotBlank(message = "키워드를 입력해주세요.") @RequestParam String keyword
   ) {
 
--- src/main/java/com/example/qnacomunity/type/SearchOrder.java
+++
@@ -1,5 +0,0 @@
-package com.example.qnacomunity.type;
-
-public enum SearchOrder {
-  CREATION_TIME, CORRECT_WORD
-}
