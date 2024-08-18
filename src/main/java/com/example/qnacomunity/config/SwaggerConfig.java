a0ff680b792f048fca8d3540b14e669492766cce
baby <gkje123@naver.com>
2024-08-18 18:22:10 +0900

    refactor: swagger 헤더 받기(JWT)

--- src/main/java/com/example/qnacomunity/config/SwaggerConfig.java
+++ src/main/java/com/example/qnacomunity/config/SwaggerConfig.java
@@ -1,13 +1,31 @@
 package com.example.qnacomunity.config;
 
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+  @Bean
+  public OperationCustomizer operationCustomizer() {
+    return (operation, handlerMethod) -> {
+      operation.addParametersItem(
+          new Parameter()
+              .in(ParameterIn.HEADER.toString())
+              .required(false)
+              .description("JWT 토큰")
+              .name("TOKEN")
+      );
+
+      return operation;
+    };
+  }
 
   @Bean
   public OpenAPI openAPI() {
